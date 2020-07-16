d8a4ad161c09ae51235d76e9c0bbebf4fb892ccb
bewithgun <ommore501@gmail.com>
2020-07-16 11:26:05 +0530

    Add Contact Component , switch to dark theme, fix some styling, etc...

--- src/app/Header/app-header.component.ts
+++ src/app/Header/app-header.component.ts
@@ -1,27 +1,48 @@
-import {Component,HostListener,ViewChild} from '@angular/core';
+import {Component,HostListener,ViewChild,ElementRef,Renderer2, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './app-header.component.html',
     styleUrls : ['./app-header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    innerWidth : any;
+    ngOnInit() {
+        this.innerWidth = window.innerWidth;
+    }
+    constructor(private renderer: Renderer2){}
+
+    @ViewChild('profile_image_header') profile_image_header: ElementRef;
+    @ViewChild('nav_container_header') nav_container_header: ElementRef;
+    @ViewChild('header') header: ElementRef;
+
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+    this.innerWidth = window.innerWidth;
+    }
 
     @HostListener("window:scroll", []) onWindowScroll() {
-
+        let headerData = {
+            height: this.header.nativeElement.style.height,
+            heightAfterScrollDown: (parseInt(this.header.nativeElement.style.height)/30).toString()+"px"
+        }
     const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (number > 50) {
-        document.getElementById("header").style.height = "80px";
-        (<HTMLElement>document.getElementsByClassName("profile-image-header")[0]).style.height = "0";
-        (<HTMLElement>document.getElementsByClassName("profile-image-header")[0]).style.border = "none";
-        (<HTMLElement>document.getElementsByClassName("nav-container-header")[0]).style.marginTop="0";
+    if (number > 50 && this.innerWidth > 900) {
+        
+        this.renderer.setStyle(this.header.nativeElement,"height","60px")
+        this.renderer.setStyle(this.profile_image_header.nativeElement,"height","40%");
+        this.renderer.setStyle(this.profile_image_header.nativeElement,"marginTop","20px");
+        this.renderer.setStyle(this.nav_container_header.nativeElement,"marginTop","20px");
+
     }
     else 
         {
-            document.getElementById("header").style.height = "280px";
-        (<HTMLElement>document.getElementsByClassName("profile-image-header")[0]).style.height = "60%";
-            (<HTMLElement>document.getElementsByClassName("profile-image-header")[0]).style.border = "4px solid black";
-            (<HTMLElement>document.getElementsByClassName("nav-container-header")[0]).style.marginTop="30px";
+            //.. Match for mobile view
+            this.renderer.setStyle(this.header.nativeElement,"height","240px");
+            this.renderer.setStyle(this.profile_image_header.nativeElement,"height","60%");
+            this.renderer.setStyle(this.nav_container_header.nativeElement,"marginTop","40px");;
         }
   }
 }
