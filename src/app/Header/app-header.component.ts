f1527e90a23223ffd3b5a445a6478a0018736a12
bewithgun <ommore501@gmail.com>
2020-07-16 13:16:10 +0530

    Support for landscape and make header more responsive.

--- src/app/Header/app-header.component.ts
+++ src/app/Header/app-header.component.ts
@@ -21,24 +21,56 @@ export class HeaderComponent implements OnInit {
     @HostListener('window:resize', ['$event'])
     onResize(event) {
     this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth)
     }
 
     @HostListener("window:scroll", []) onWindowScroll() {
     const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (number > 50 && this.innerWidth > 900) {
-        
-        this.renderer.setStyle(this.header.nativeElement,"height","60px")
-        this.renderer.setStyle(this.profile_image_header.nativeElement,"height","40%");
-        this.renderer.setStyle(this.profile_image_header.nativeElement,"marginTop","20px");
-        this.renderer.setStyle(this.nav_container_header.nativeElement,"marginTop","20px");
-
-    }
-    else 
-        {
-            //.. Match for mobile view
-            this.renderer.setStyle(this.header.nativeElement,"height","240px");
-            this.renderer.setStyle(this.profile_image_header.nativeElement,"height","60%");
-            this.renderer.setStyle(this.nav_container_header.nativeElement,"marginTop","40px");;
+    if (number > 50) { //scrolled down
+        console.log("You have scrolled down")
+        if (this.innerWidth > 900){ //desktop
+            console.log("and its desktop")
+            this.renderer.setStyle(this.header.nativeElement,"height","60px")
+            this.renderer.setStyle(this.profile_image_header.nativeElement,"height","40%");
+            this.renderer.setStyle(this.profile_image_header.nativeElement,"marginTop","20px");
+            this.renderer.setStyle(this.nav_container_header.nativeElement,"marginTop","20px");
         }
-  }
+        else if (this.innerWidth < 900 && this.innerWidth > 500) { // mobile_lanscape
+            console.log("and its mobile with lanscape")
+            this.renderer.setStyle(this.header.nativeElement,"height","60px")
+
+        }
+        else if(this.innerWidth < 500) // mobile_potrait
+        {
+            this.renderer.setStyle(this.header.nativeElement,"height","60px")
+
+            console.log("and its mobile with portrait")
+
+        }
+    }
+    else{ // scrool back to normal
+        console.log("You have scrolled up")
+            if (this.innerWidth > 900){ //desktop
+            console.log("and its desktop")
+
+                this.renderer.setStyle(this.header.nativeElement,"height","240px");
+                this.renderer.setStyle(this.profile_image_header.nativeElement,"height","60%");
+                this.renderer.setStyle(this.nav_container_header.nativeElement,"marginTop","40px");
+            }
+            else if(this.innerWidth < 900 && this.innerWidth > 500) { // mobile_lanscape
+            console.log("and its mobile with lanscape")
+            this.renderer.setStyle(this.header.nativeElement,"height","130px");
+
+
+            }
+            else if(this.innerWidth < 500) // mobile_potrait
+            {
+                this.renderer.setStyle(this.header.nativeElement,"height","130px");
+
+            console.log("and its mobile with portrait")
+                
+            }
+        }
+            
+    }
 }
